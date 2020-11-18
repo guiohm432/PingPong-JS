1b64dcfe879b6fff37b553863fa45f59c3ab0b39
Guillaume Marcellin <marcellinguillaume@hotmail.fr>
2020-11-18 09:32:06 +0100

    CREATE 4 SOUND VARIABLES
    These variables store an HTML audio tag created with the JS Audio() constructor with a specific URL as a src

--- pong.js
+++ pong.js
@@ -1,2 +1,13 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext('2d');
+
+// load sounds
+let hit = new Audio();
+let wall = new Audio();
+let userScore = new Audio();
+let comScore = new Audio();
+
+hit.src = "sounds/hit.mp3";
+wall.src = "sounds/wall.mp3";
+comScore.src = "sounds/comScore.mp3";
+userScore.src = "sounds/userScore.mp3";
